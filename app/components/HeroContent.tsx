d31a1108362d1a3f3778c3e26d0d8dce3848c410
KhalifRafi <71247773+KhalifRafi@users.noreply.github.com>
2024-03-01 13:46:24 +0700

    update font

--- app/components/HeroContent.tsx
+++ app/components/HeroContent.tsx
@@ -16,7 +16,7 @@ const HeroContent = () => {
         <div className='flex justify-center items-center h-full' data-aos="fade-up">
             <div className="flex flex-col gap-y-4 z-10 text-center sm:max-w-[768px] sm:gap-y-8">
                 <div className='flex flex-col gap-y-3'>
-                    <p className="text-white text-sm px-2 xs:text-xl lg:text-3xl sm:px-8">PT. INTERNASIONAL <br className='sm:hidden' /> HARAMAIN MAKKAH</p>
+                    <p className="text-white text-lg px-2 xs:text-xl lg:text-3xl sm:px-8">PT. INTERNASIONAL <br className='sm:hidden' /> HARAMAIN MAKKAH</p>
                     <p className='text-white text-sm px-2 xs:text-base lg:text-2xl sm:px-8'>PPIU No. 2071220059084003</p>
                 </div>
                 <h1 className="text-5xl font-semibold text-white max-md:text-lg max-lg:px-8">LAYANAN UMRAH BERKUALITAS DAN TERBAIK UNTUK ANDA</h1>
