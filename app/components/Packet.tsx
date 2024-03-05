696f9999ca4dbc5a4d6807da323c215d9ab7c445
KhalifRafi <71247773+KhalifRafi@users.noreply.github.com>
2024-03-05 16:24:07 +0700

    perbaikan kode v1 (paket)

--- app/components/Packet.tsx
+++ app/components/Packet.tsx
@@ -1,11 +1,16 @@
-import PacketContent from "./PacketContent"
+import PacketCard from "./PacketCard"
 
 const Packet = () => {
     return (
         <section className="w-full h-auto bg-[#EEEDEB] px-20 py-8 flex flex-col gap-y-7 max-lg:px-4" id="packet">
             <h1 className="text-3xl text-black text-center mb-10">Paket Umroh <br className="lg:hidden" /> In-Haramain Tour</h1>
 
-            <PacketContent />
+            <div className="grid grid-cols-1 gap-x-12 md:grid-cols-2 xl:grid-cols-3 gap-y-8">
+                <PacketCard dataAos="zoom-in-down" src="paketreguler.jpg" programDay="Program 9 Hari" title="UMROH REGULER 9 HARI" price="Rp. 27.900.000" include1="Visa Umroh" include2="Tiket Ekonomi Class PP" include3="Hotel" include4="Makan" include5="Asuransi" include6="Perlengkapan Umroh" href="/paket1" />
+                <PacketCard dataAos="zoom-in-up" src="paketreguler12.jpg" programDay="Program 12 Hari" title="UMROH REGULER 12 HARI" price="Rp. 30.900.000" include1="Visa Umroh" include2="Tiket Ekonomi Class PP" include3="Hotel" include4="TL & Muthawif" include5="Asuransi" include6="Perlengkapan Umroh" href="/paket1" />
+                <PacketCard dataAos="zoom-in-down" src="umrohturki.jpg" programDay="Program 12 Hari" title="UMROH PLUS TURKI" price="Rp. 28.000.000" include1="Visa Umroh" include2="Tiket Ekonomi Class PP" include3="Hotel" include4="Paspor" include5="Asuransi" include6="Tips Guide Turki" href="/paket3" />
+                <PacketCard dataAos="zoom-in-up" src="umrohsuper.jpeg" programDay="Program 9 Hari" title="UMROH SUPER PROMO" price="Rp. 22.750.000" include1="Visa Umroh" include2="Tiket Ekonomi Class PP" include3="Hotel" include4="Bus AC" include5="Mottowif" include6="Tour Leader" href="/paket2" />
+            </div>
         </section>
     )
 }
---
+++ app/components/PacketCard.tsx
@@ -0,0 +1,88 @@
+"use client"
+
+import { Card, CardHeader, CardBody, Image } from "@nextui-org/react"
+import { IoIosCheckmarkCircleOutline } from "react-icons/io"
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
+import Link from "next/link";
+
+interface CardProps {
+    dataAos: string,
+    src: string,
+    programDay: string,
+    title: string,
+    price: string,
+    include1: string,
+    include2: string,
+    include3: string,
+    include4: string,
+    include5: string,
+    include6: string,
+    href: string
+}
+
+const PacketCard = ({ dataAos, src, programDay, title, price, include1, include2, include3, include4, include5, include6, href }: CardProps) => {
+    useEffect(() => {
+        AOS.init({
+            duration: 800,
+            once: false,
+        })
+    }, [])
+    return (
+        <Card className="py-4 w-fit mx-auto" data-aos={dataAos}>
+            <CardHeader className="overflow-visible py-2">
+                <Image
+                    alt="Card background"
+                    className="object-cover rounded-xl"
+                    src={`/images/${src}`}
+                    width={360}
+                />
+            </CardHeader>
+            <CardBody className="pb-0 pt-2 px-4 flex-col items-start">
+                <p className="text-tiny uppercase font-bold">{programDay}</p>
+                <small className="text-default-500">Agustus - September 2024</small>
+                <h4 className="font-bold text-large">{title} <span className="text-lg text-black font-normal">/ {price}</span></h4>
+
+                <div className="flex flex-col gap-y-4 mt-3">
+                    <h3>Harga Sudah Termasuk :</h3>
+                    <div className="grid grid-cols-2">
+                        <div className="flex items-center gap-x-1">
+                            <IoIosCheckmarkCircleOutline size={35} className="text-[#0B8457] max-sm:w-7" />
+                            <p className="text-black text-base max-md:text-sm">{include1}</p>
+                        </div>
+                        <div className="flex items-center gap-x-1 ml-[-40px]">
+                            <IoIosCheckmarkCircleOutline size={35} className="text-[#0B8457] max-sm:w-7" />
+                            <p className="text-black text-base max-md:text-sm">{include2}</p>
+                        </div>
+                        <div className="flex items-center gap-x-1">
+                            <IoIosCheckmarkCircleOutline size={35} className="text-[#0B8457] max-sm:w-7" />
+                            <p className="text-black text-base max-md:text-sm">{include3}</p>
+                        </div>
+                        <div className="flex items-center gap-x-1 ml-[-40px]">
+                            <IoIosCheckmarkCircleOutline size={35} className="text-[#0B8457] max-sm:w-7" />
+                            <p className="text-black text-base max-md:text-sm">{include4}</p>
+                        </div>
+                        <div className="flex items-center gap-x-1">
+                            <IoIosCheckmarkCircleOutline size={35} className="text-[#0B8457] max-sm:w-7" />
+                            <p className="text-black text-base max-md:text-sm">{include5}</p>
+                        </div>
+                        <div className="flex items-center gap-x-1 ml-[-40px]">
+                            <IoIosCheckmarkCircleOutline size={35} className="text-[#0B8457] max-sm:w-7" />
+                            <p className="text-black text-base max-md:text-sm">{include6}</p>
+                        </div>
+                        <div className="flex items-center gap-x-1">
+                            <IoIosCheckmarkCircleOutline size={35} className="text-[#0B8457] max-sm:w-7" />
+                            <p className="text-black text-base max-md:text-sm">dll</p>
+                        </div>
+                    </div>
+                    <Link href={href} className="px-3 py-2 bg-[#EAC100] rounded-lg text-white w-fit">
+                        Detail Paket
+                    </Link>
+                </div>
+            </CardBody>
+        </Card>
+    )
+}
+
+export default PacketCard
